b2577916a44773e8686c65f969670d48d530125d
Elmira <elia02_03@mail.ru>
2022-12-20 21:36:12 +0500

    Ex10 sem 2

--- Ex10 sem 2 v2 copy/Program.cs
+++ Ex10 sem 2 v2 copy/Program.cs
@@ -6,7 +6,8 @@ Console.WriteLine("Введите трехзначное число: ");
 int N = int.Parse(Console.ReadLine());
 int b = 0;
 if (N >= 100 && N < 1000)
+{
     b = (N / 10) % 10;
+    Console.WriteLine($"Вторая цифра Вашего числа: {b}");
+}
 else Console.WriteLine("число кривое");
-
-Console.WriteLine($"Вторая цифра Вашего числа: {b}");
