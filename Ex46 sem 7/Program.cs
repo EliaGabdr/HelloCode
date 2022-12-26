ed1be88cbffcfc383d305fa3a873f30431f2012a
Elmira <elia02_03@mail.ru>
2022-12-27 00:21:20 +0500

    Ex 46 sem 7

---
+++ Ex46 sem 7/Program.cs
@@ -0,0 +1,41 @@
+﻿// Задайте двумерный массив размера m на n, каждый элемент в массиве находится по формуле: Aₘₙ = m+n. 
+// Выведите полученный массив на экран.
+
+int m = ReadInt("m");
+int n = ReadInt("n");
+
+int[,] newArray = GetArray(m, n);
+PrintArray(newArray);
+
+
+
+int ReadInt(string argument)
+{
+    Console.Write($"Input {argument}: ");
+    return int.Parse(Console.ReadLine()!);
+}
+
+int[,] GetArray(int length, int secondLength)
+{
+    int[,] array = new int[length, secondLength];
+    for (int i = 0; i < length; i++)
+    {
+        for (int j = 0; j < secondLength; j++)
+        {
+            array[i, j] = i + j;
+        }
+    }
+    return array;
+}
+
+void PrintArray(int[,] array)
+{
+    for (int i = 0; i < array.GetLength(0); i++)
+    {
+        for (int j = 0; j < array.GetLength(1); j++)
+        {
+            Console.Write($"{array[i, j]} ");
+        }
+        Console.WriteLine();
+    }
+}
