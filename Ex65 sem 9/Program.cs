539ae85bc257061d6c282716e489a0cc0bbd2274
Elmira <elia02_03@mail.ru>
2023-01-02 13:02:23 +0500

    Ex 63 sem 9

---
+++ Ex65 sem 9/Program.cs
@@ -0,0 +1,12 @@
+﻿// от N до M
+void PrintFromUntil(int M, int N)
+{
+    if (M >= N)
+    {
+        Console.WriteLine(M);
+        return;
+    }
+    Console.Write($"{M}, ");
+    PrintFromUntil(M + 1, N);
+}
+PrintFromUntil(4, 8);
