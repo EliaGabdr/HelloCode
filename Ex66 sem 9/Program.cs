fef0bba17b9fc55040ef660b8a818177d6e3d462
Elmira <elia02_03@mail.ru>
2023-01-05 18:47:01 +0500

    Ex 66 sem 9

---
+++ Ex66 sem 9/Program.cs
@@ -0,0 +1,16 @@
+﻿// Задача 66: Задайте значения M и N. Напишите программу, которая найдёт сумму натуральных элементов в промежутке от M до N.
+// M = 1; N = 15 -> 120
+// M = 4; N = 8. -> 30
+
+void SumInteger(int m, int n, int sum)
+{
+    sum += n;
+    if (n <= m)
+    {
+        System.Console.Write($"{sum}");
+        return;
+    }
+    SumInteger(m, n - 1, sum);
+}
+SumInteger(4, 8, 0);
+
